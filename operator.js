9d3457a99f20887f33152552390b72d2c0817177
이소라 Sora Lee <46148636+srlee010@users.noreply.github.com>
2022-11-21 23:29:30 +0900

    Update operator.js

--- operator.js
+++ operator.js
@@ -101,3 +101,97 @@ console.log('' == false);   // empty 문자열 False니까 출력값은 True
 console.log('' === false);   // empty 문자열은 boolean type 아니어서 ===이용하면 False
 console.log(null == undefined);   // null 과 undefined은 같은것으로 간주 True
 console.log(null === undefined);    // null 과 undifined은 서로 다른 type이라 False
+
+
+// 8. If operators
+// if, else if, else
+const name = 'sora';
+if (name === 'sora') {
+  console.log('Welcome, Sora!');
+} else if (name === 'coder') {
+  console.log('you are an amazing coder.')
+} else {
+  console.log('unknown');
+}
+
+
+// 9. Ternary operators: ? 
+// condition ? value 1 : value2; (간단할때만 사용하기)
+// statement가 True인가(?) 맞다면 yes, 아니라면(:) no를 출력해 
+console.log(name === 'sora' ? 'yes' : 'no');
+
+
+// 10. Switch statement
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = 'chrome'
+switch (browser) {
+  case 'IE':
+    console.log('go away!');
+    break;
+  case 'Chrome':
+  case 'Firefox':
+    console.log('Love you!');
+    break;
+  default:
+    console.log('same all!');
+    break;
+}
+
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is executed.
+let i = 3;
+while (i > 0) {
+  console.log(`while: ${i}`);
+  i--;
+}
+
+
+// do while loop, body code is executed first,
+// then check the condition.
+// 블럭을 실행한 다음 조건이 맞는지 검사함 i 가 0
+do {
+  console.log(`do while: ${i}`);
+  i--;
+} while (i > 0);
+
+// for loop
+// for(begin; condition; step)
+// 시작하는 문장, condition은 중간에, 마지막으로 어떤 스텝을 밟아나갈건지 명시하기
+for (i = 3; i > 0; i--) {
+  console.log(`for ${i}`);
+}
+
+for (let i = 3; i > 0; i = i -2) {
+  //inline variable declaration
+  console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+// while이나 For는 서로 nesting해서 작성할 수 있다
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`i ${i}, j:${j}`);
+  }
+}
+
+
+// break, continue
+// 01. iterate from 0 to 10 and print only even numbers (use continue)
+for (let a = 0; i < 11; a++) {
+  if (a % 2 === 0) {
+    continue;
+  }
+    console.log(`q1. ${a}`);
+} 
+
+// 02. iterate from 0 to 10 and print numbers until reaching 8 (use break)
+for (let i = 0; i < 11; i++) {
+  if (i > 8) {
+    break;
+  }
+  console.log(`q2. ${i}`);
+}
