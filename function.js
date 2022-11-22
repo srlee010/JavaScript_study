3afa8320a4ac5f18b9a3a1e7e19e2705dba3ac41
이소라 Sora Lee <46148636+srlee010@users.noreply.github.com>
2022-11-22 23:27:49 +0900

    Update function.js

--- function.js
+++ function.js
@@ -97,3 +97,67 @@ function upgradeUser(user) {
   }
     // long upgrade logic ...
 }
+
+
+
+// First-class function
+// functions are treated like any other variable
+// can be assgined as a value to variable
+// can be passed as an argumnet to other functions.
+// can be returned by another function
+
+// 1. Function expression
+// a function declaration can be called earlier than it is defined. (hoisted)
+// a function expression is created when the execution reaches it.
+
+// 함수를 선언함과 동시에 print라는 변수에 할당
+const print = function () {   // anonymous function: function에 이름이 없고 function이라는 키워드를 이용해서 parameter와 블럭을 이용
+  console.log('print!');  
+} 
+print();
+const printAgain = print;
+printAgain(); 
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+
+// 2. Callback function using function expression
+function randomQuiz (answer, printYes, printNo) {
+  if (answer === 'love you') {
+    printYes();
+  } else {
+    printNo();
+  }
+}
+// anonymous function
+const printYes = function () { 
+  console.log('correct!');
+};
+// named function
+// better debugging in debugger's stack traces
+// recursions (재귀: 함수안에서 자신 스스로를 호출하는 것)
+const printNo = function print () {
+  console.log('No think again~');
+};
+
+randomQuiz('love you', printYes, printNo);
+randomQuiz('hate you', printYes, printNo);
+
+
+// Arrow function
+// always anonymous
+// const simplePrint = function() {
+// console.log('simplePrint!');
+// };
+
+const simplePrint = () => console.log('simplePrint!');
+const add = (a , b) => a + b;
+const simpleMultiply = (a , b) => {
+  // do something more
+  return a * b;
+} 
+
+// IIFE: Immediately Invoked Function Expressin
+(function hello() {
+  console.log('IIFE');
+})();
