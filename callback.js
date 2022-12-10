28abeebcdd3a5fe82108b6cf9b0142435efd1e6c
이소라 Sora Lee <46148636+srlee010@users.noreply.github.com>
2022-12-10 16:59:43 +0900

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// JavaScript is synchronous (동기식: 순서대로 실행)
+// Execute the code block by order after hoisting. (순서대로 코드실행)
+// hoisting: var, function declaration (함수 선언이 제일 위로 올라가는 것) 
+console.log('1');
+console.log('2');
+console.log('3');
+
+setTimeout()
+// 지정한 시간이 지나면 우리가 전달한 콜백함수를 호출하는 API 
+// 콜백함수? 우리가 전달한 함수를 나중에 호출해줘
+
+
+비동기적 (언제 코드가 실행되는지 예측할 수 없는 걸 의미)
